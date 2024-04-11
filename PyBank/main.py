56ed433a5d96b45ab95fca28e3fff61c152e3883
JNJJNJ <jaysingh@ku.edu>
2024-04-10 21:13:01 -0500

    Month Total Reorganized

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,26 +9,21 @@ import csv
 ### start here
 #create variables
 budget_file_path = r"PyBank\Resources\budget_data.csv"
+total_months = 0
+net_total = 0
+changes = []
+dates = []
 # open the file
 with open(budget_file_path) as budget_file:
     csv_file = csv.reader(budget_file)
+    next(csv_file)
     #read row in the file
     for row in csv_file:
-        #total number of months
-        months = []
-        profit_losses = []
-        changes = []
-
-        for row in csv_file:
-            months.append(row[0])
-
-profit_losses.append(int(row[1]))
+        total_months += 1
 
 # calculate the total number of months
-total_months = len(months)
+print(total_months)
 
-
-# calculate the changes in profit/losses and store them in a list
 
 ### end here
 
